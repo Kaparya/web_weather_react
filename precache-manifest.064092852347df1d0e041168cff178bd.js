791ca5deb6f22b30445b08c46bbb1c2fc768b929
Kaparya <kaparyaka.dev@gmail.com>
2025-05-16 23:33:46 +0300

    Updates

---
+++ precache-manifest.064092852347df1d0e041168cff178bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d474b9ca1bd8b56b4630ffeb77bc9dad",
+    "url": "/web_weather_react/index.html"
+  },
+  {
+    "revision": "ee9a5cc24ef3f5a80807",
+    "url": "/web_weather_react/static/css/main.ff38f921.chunk.css"
+  },
+  {
+    "revision": "6593f894d016e85388cb",
+    "url": "/web_weather_react/static/js/2.c7821bdd.chunk.js"
+  },
+  {
+    "revision": "ee9a5cc24ef3f5a80807",
+    "url": "/web_weather_react/static/js/main.63fc8d12.chunk.js"
+  },
+  {
+    "revision": "0be995681461c22399ee",
+    "url": "/web_weather_react/static/js/runtime~main.4fe1365b.js"
+  }
+]);
